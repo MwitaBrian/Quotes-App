d2f2761aa193f7973e5a593d452c63304fcad1bd
MwitaBrian <brianmwita13@gmail.com>
2022-02-08 16:06:09 +0300

    Display Quote details using input property binding

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [ 
-    {id:1, quote:'Manners maketh a man.', author:'William Horman'}, 
-    {id:2, quote:'The way to get started is to quit talking and begin doing.', author:'Walt Disney'}, 
-    {id:3, quote:"Try to be a rainbow in someone's cloud.", author:'Maya Angelou'}
+    {id:1, quote:'Manners maketh a man.', author:'William Horman', name:'Mwita'}, 
+    {id:2, quote:'The way to get started is to quit talking and begin doing.', author:'Walt Disney', name:'Brian'}, 
+    {id:3, quote:"Try to be a rainbow in someone's cloud.", author:'Maya Angelou', name:'Adams'}
   ];
 
   constructor() { }
