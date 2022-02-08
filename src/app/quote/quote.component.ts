e2380213d21549053848e1d202b5542c9e61f5a4
MwitaBrian <brianmwita13@gmail.com>
2022-02-08 12:54:52 +0300

    Add and display Quotes Array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [ 
+    {id:1, quote:'Manners maketh a man.', author:'William Horman'}, 
+    {id:2, quote:'The way to get started is to quit talking and begin doing.', author:'Walt Disney'}, 
+    {id:3, quote:"Try to be a rainbow in someone's cloud.", author:'Maya Angelou'}
+  ];
 
   constructor() { }
 
